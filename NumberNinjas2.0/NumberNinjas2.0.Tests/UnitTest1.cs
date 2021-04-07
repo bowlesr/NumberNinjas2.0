02b68430f7a584e7a0bc62f9cdc1dd173799b3e3
bowlesr <80800355+bowlesr@users.noreply.github.com>
2021-04-07 15:38:19 -0400

    Initial commit

---
+++ NumberNinjas2.0/NumberNinjas2.0.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace NumberNinjas2._0.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ NumberNinjas2.0/NumberNinjas2.0/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NumberNinjas2._0.Startup))]
+namespace NumberNinjas2._0
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
